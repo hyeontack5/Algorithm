3d0ecfc22d0dbe0518d48e203c2799828e22a2d3
hyeontack5 <sksgusxor@gmail.com>
2021-09-02 16:17:09 +0900

    Add: 출력변환

---
+++ Python/CodeUp/6028_10진 정수 입력받아 16진수로 출력하기2.py
@@ -0,0 +1,10 @@
+num = int(input())
+
+# print('%x'% num)
+# %x로 출력하면 16진수(hexadecimal) 소문자로 출력됩니다.
+
+print('%X'% num)
+# %X로 출력하면 16진수(hexadecimal) 대문자로 출력됩니다.
+
+# print('%o'% num)
+# %o로 출력하면 8진수(octal) 문자열로 출력됩니다.
