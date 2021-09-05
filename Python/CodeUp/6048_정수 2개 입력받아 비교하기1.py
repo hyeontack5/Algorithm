b6dbe4b47f9d0d19a42f65479516920dd4d97a4d
hyeontack5 <sksgusxor@gmail.com>
2021-09-06 00:13:27 +0900

    Add: 비교연산

---
+++ Python/CodeUp/6048_정수 2개 입력받아 비교하기1.py
@@ -0,0 +1,12 @@
+# 방법 1
+a, b = map(int, input().split()) 
+
+if (a < b): 
+  print("True") 
+else: 
+  print("False")
+
+# 방법 2
+# a, b = map(int, input().split()) 
+
+# print(a < b)
---
+++ Python/CodeUp/6051_정수 2개 입력받아 비교하기4.py
@@ -0,0 +1,12 @@
+# 방법 1
+a, b = map(int, input().split()) 
+
+if (b != a):
+  print("True")
+else:
+  print("False")
+
+# 방법 2
+# a, b = map(int, input().split()) 
+
+# print(b != a)
