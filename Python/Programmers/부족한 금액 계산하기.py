536f5dda5e5045c606ca29c59b1bf7a11211bfdd
hyeontack5 <sksgusxor@gmail.com>
2021-11-03 20:14:52 +0900

    Add: Level 1 - 부족한 금액 계산하기

---
+++ Python/Programmers/부족한 금액 계산하기.py
@@ -0,0 +1,12 @@
+def solution(price, money, count):
+    answer = 0
+    
+    for i in range(1, count+1):
+        money -= (price * i)
+    
+    if (money < 0):
+        answer = -money
+    else:
+        answer = 0
+    
+    return answer
