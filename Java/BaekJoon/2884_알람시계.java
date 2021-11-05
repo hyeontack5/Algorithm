b0dec0524e38b2ea850cd34681b2cb75ca762215
hyeontack5 <sksgusxor@gmail.com>
2021-11-05 13:31:48 +0900

    Add: 2884_알람시계

--- Java/BaekJoon/2884_알람시계.java
+++ Java/BaekJoon/2884_알람시계.java
@@ -1,5 +1,32 @@
-package Java.BaekJoon;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class 2884_알람시계 {
-  
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int hour = Integer.parseInt(st.nextToken());
+        int min = Integer.parseInt(st.nextToken());
+
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+
+        if (min < 45) {
+            if (hour == 0) {
+                hour = 23;
+                sb.append(hour).append(" ");
+            } else {
+                hour -= 1;
+                sb.append(hour).append(" ");
+            }
+            sb.append(min = 60 - (45 - min));
+        } else {
+            sb.append(hour).append(" ").append(min - 45);
+        }
+
+        System.out.println(sb);
+    }
 }
