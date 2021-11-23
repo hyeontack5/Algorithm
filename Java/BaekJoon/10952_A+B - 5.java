cdbac759991a87de5b53c19b0ed2c5746deb5616
hyeontack5 <sksgusxor@gmail.com>
2021-11-23 23:21:15 +0900

    Style: 개행 제거

--- Java/BaekJoon/10952_A+B - 5.java
+++ Java/BaekJoon/10952_A+B - 5.java
@@ -4,7 +4,6 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
