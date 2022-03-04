079e98607f1a3a0c78a301b595be2047e41ff20d
hyeontack5 <sksgusxor@gmail.com>
2022-03-04 22:37:25 +0900

    Add: 이것이 코딩 테스트다 풀이

--- Python/BaekJoon/18406_럭키 스트레이트.py
+++ Python/BaekJoon/18406_럭키 스트레이트.py
@@ -12,3 +12,23 @@ if (left == right):
     print("LUCKY")
 else:
     print("READY")
+
+# 방법 2
+n = input()
+
+length = len(n) # 점수값의 총 자릿수
+summary = 0
+
+# 왼쪽 부분의 자릿수 합 더하기
+for i in range(length // 2):
+    summay += int(n[i])
+
+# 오른쪽 부분의 자릿수 합 빼기
+for j in range(length // 2, length):
+    summary -= int(n[j])
+
+# 왼쪽 부분과 오른쪽 부분의 자릿수 합이 동일한지 검사
+if (summary == 0):
+    print("LUCKY")
+else:
+    print("READY")
