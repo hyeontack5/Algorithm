caf8a771c204a2b6a6241c0024c42e8f1f74fc1f
hyeontack5 <sksgusxor@gmail.com>
2022-03-16 16:03:23 +0900

    데크 자료형을 이용한 최적화

--- Python/LeetCode/125_Valid Palindrome.py
+++ Python/LeetCode/125_Valid Palindrome.py
@@ -12,3 +12,19 @@ def isPalindrome(self, s: str) -> bool:
             return False
     
     return True
+
+# 데크 자료형을 이용한 최적화
+
+def isPalindrome(self, s: str) -> bool:
+    # 자료형 데크로 선언
+    strs: Deque = collections.deque()
+
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+
+    while len(strs) > 1:
+        if strs.popleft() != strs.pop():
+            return False
+    
+    return True
