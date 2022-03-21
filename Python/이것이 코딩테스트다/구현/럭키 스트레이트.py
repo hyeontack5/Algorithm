4259e48bcc6b348bf39908bdb57454b4873ad406
hyeontack5 <sksgusxor@gmail.com>
2022-03-21 23:13:05 +0900

    Add: 해설 풀이

--- Python/이것이 코딩테스트다/구현/럭키 스트레이트.py
+++ Python/이것이 코딩테스트다/구현/럭키 스트레이트.py
@@ -16,3 +16,24 @@ if (left_score == right_score):
     print("LUCKY")
 else:
     print("READY")
+
+# 방법 2
+
+# 현재 점수 n
+n = input()
+length = len(n) # 점수값의 총 자릿수
+summary = 0
+
+# 왼쪽 부분의 자릿수 합 더하기
+for i in range(length // 2):
+    summary += int(n[i])
+
+# 오른쪽 부분의 자릿수 합 빼기
+for i in range(length // 2, length):
+    summary -= int(n[i])
+
+# 왼쪽 부분과 오른쪽 부분의 자릿수 합이 동일한지 검사
+if summary == 0:
+    print("LUCKY")
+else:
+    print("READY")
