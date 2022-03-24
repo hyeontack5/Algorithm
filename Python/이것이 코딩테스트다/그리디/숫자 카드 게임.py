83364abec9efc66e7511e17e6e48e1f6ad2e498c
hyeontack5 <sksgusxor@gmail.com>
2022-03-24 23:31:35 +0900

    Add: 2중 반복문 방법

--- Python/이것이 코딩테스트다/그리디/숫자 카드 게임.py
+++ Python/이것이 코딩테스트다/그리디/숫자 카드 게임.py
@@ -15,3 +15,23 @@ for i in range(n):
 
 # 최종 답안 출력
 print(result)
+
+# 2중 반복문을 이용한 방법
+
+# 행의 개수 n, 열의 개수 m
+n, m = map(int, input().split())
+
+result = 0
+
+# 한 줄씩 입력받아 확인
+for i in range(n):
+    data = list(map(int, input().split()))
+    # 현재 줄에서 가장 작은 수 찾기
+    min_value = 10001
+    for a in data:
+        min_value = min(min_value, a)
+    # 가장 작은 수들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
+
+# 최종 답안 출력
+print(result)
